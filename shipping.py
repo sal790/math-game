c825f25df6c20f1cd45338d09f511faf2b98af9c
sal790 <salalabid22@gmail.com>
2025-08-08 10:54:01 +0100

    Update shipping.py

--- shipping.py
+++ shipping.py
@@ -1,18 +1,46 @@
-print("Welcome to the do-list manager")
+print("Welcome to the to-do list manager")
 
-todo_list=[]
+todo_list = []
 
 while True:
-    print("\n choose an option:")
-    print("1.Add a task")
-    print("2.View task")
-    print("3.Remove task")
-    print("4.Exit")
-     choice= input(" Enter your choice (1,2,3,4)")
+    print("\nChoose an option:")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Remove a task")
+    print("4. Exit")
 
+    choice = input("Enter your choice (1, 2, 3, 4): ")
 
-     if choice == "1":
-         task=input("enter a task")
-         todo_list.append(task)
-         print(f"{task} has been added")
+    if choice == "1":
+        task = input("Enter a task: ")
+        todo_list.append(task)
+        print(f"{task} has been added.")
+    elif choice == "2":
+        if not todo_list:
+            print("your list is empty")
+        else :
+            print("your to do list ")
+            for i, task in enumerate(todo_list,1):
+                print(f"{i}.{task}")
+    elif choice=="3":
+        if not todo_list:
+            print("your list is empty")
+        else :
+            print("your to do list ")
+            for i, task in enumerate(todo_list,1):
+                print(f"{i}.{task}")
+            try:
+                task_num= int(input("enter a task number to remove"))
+                if 1 <= task_num <= len(todo_list):
+                   removed = todo_list.pop(task_num - 1)
+                   print(f"'{removed}' has been removed.")
+                else:
+                     print("Invalid task number.")
+            except ValueError:
+             print("Please enter a valid number.")
+    elif choice=="4":
+        print("Good Bye")
+        break
+         
+
 
